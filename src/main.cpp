ce19b2552754468067b673f586d8b0d4ab062edb
Danilo Medeiros <danilomedeiros03@gmail.com>
2018-12-09 14:13:31 -0300

    finalizadas funcoes eletivas * Separação em Canais * Equalização de histograma

--- src/main.cpp
+++ src/main.cpp
@@ -25,20 +25,19 @@ struct Img {
 };
 
 void abre_img(char nome[], Img& img);
-
-void salva_img(char nome[], Img& img);
-
+void salva_img(char nome[], Img img);
 int ler_escolha_menu();
 
 // Funcoes:
 
+void computa_negativo(Img img_in, Img& img_out);
 void corta_rgb(Img& imagem, Img& img_saida, int li, int ci, int lf, int cf);
 void binariza_canal(Img& img_in, Img& img_out, int limiar);
 void diminui_tamanho(Img& img_in, Img& img_out);
 void reflete(Img& img_in, Img& img_out);
 void borra_imagem(Img& img_in, Img& img_out);
 void equaliza_histograma(Img& img_in, Img& img_out);
-
+void separa_canais(Img& img_in, Img& img_r, Img& img_g, Img& img_b);
 
 int main() {
   cout << "\nProcessador de imagens ADL\n\nEscolha uma funcao:\n\n";
@@ -54,18 +53,32 @@ int main() {
     Img img_saida;
 
     switch (escolha) {
-      case 0: // Sair
-        rodar = false;
-        break;
-      case 1: // Negativo de imagem
-        cout << "\nFuncao nao implementada\n\n=================\n";
-        /* char nome_arquivo[TAMANHO_MAXIMO_NOME_ARQUIVO];
-        Img imagem;
+      case 1:
         abre_img(nome_arquivo, imagem);
-        salva_img(nome_arquivo, imagem); */
+        computa_negativo(imagem, img_saida);
+        salva_img(nome_arquivo, img_saida);
         break;
-      case 2: // Binarizacao de canais
-        cout << "\nFuncao nao implementada\n\n=================\n";
+      case 2:
+        Img img_r;
+        Img img_g;
+        Img img_b;
+        abre_img(nome_arquivo, imagem);
+        separa_canais(imagem, img_r, img_g, img_b);
+        char nome_r[TAMANHO_MAXIMO_NOME_ARQUIVO];
+        strcpy(nome_r, nome_arquivo);
+        strcat(nome_r, "_red");
+
+        char nome_g[TAMANHO_MAXIMO_NOME_ARQUIVO];
+        strcpy(nome_g, nome_arquivo);
+        strcat(nome_g, "_green");
+
+        char nome_b[TAMANHO_MAXIMO_NOME_ARQUIVO];
+        strcpy(nome_b, nome_arquivo);
+        strcat(nome_b, "_blue");
+
+        salva_img(nome_r, img_r);
+        salva_img(nome_g, img_g);
+        salva_img(nome_b, img_b);
         break;
       case 3:
         abre_img(nome_arquivo, imagem);
@@ -108,6 +121,14 @@ int main() {
         abre_img(nome_arquivo, imagem);
         borra_imagem(imagem, img_saida);
         salva_img(nome_arquivo, img_saida);
+        break;
+      case 8:
+        abre_img(nome_arquivo, imagem);
+        equaliza_histograma(imagem, img_saida);
+        salva_img(nome_arquivo, img_saida);
+        break;
+      default:
+        cout << "Opção inválida\n";
         break;
     }
   }
@@ -159,7 +180,7 @@ void abre_img(char nome[], Img& img) {
   arquivo.close();
 }
 
-void salva_img(char nome[], Img& img) {
+void salva_img(char nome[], Img img) {
   ofstream arquivo_saida;
 
   char nome_saida[TAMANHO_MAXIMO_NOME_ARQUIVO];
@@ -184,16 +205,31 @@ void salva_img(char nome[], Img& img) {
 
 int ler_escolha_menu() {
   cout << "[1] Negativo de imagem\n";
-  cout << "[2] Binarizacao de canais\n";
+  cout << "[2] Separação de canais\n";
   cout << "[3] Recorte de subimagem (crop)\n";
   cout << "[4] Binarização de imagem (por canal)\n";
   cout << "[5] Redução de tamanho da imagem\n";
   cout << "[6] Reflexão horizontal/vertical na imagem (mirror)\n";
   cout << "[7] Convolução para filtragem passa baixas\n";
+  cout << "[8] Equalização de histograma\n";
   cout << "[0] Sair\n";
   int escolha;
   cin >> escolha;
   return escolha;
+}
+
+void computa_negativo(Img img_in, Img& img_out) {
+  img_out.qtd_linhas = img_in.qtd_linhas;
+  img_out.qtd_colunas = img_in.qtd_colunas;
+  for (int i = 0; i < img_out.qtd_linhas; i++) {
+    for (int j = 0; j < img_out.qtd_colunas; j++) {
+      Pixel novo_pixel;
+      novo_pixel.red = 255 - img_in.matriz[i][j].red;
+      novo_pixel.green = 255 - img_in.matriz[i][j].green;
+      novo_pixel.blue = 255 - img_in.matriz[i][j].blue;
+      img_out.matriz[i][j] = novo_pixel;
+    }
+  }
 }
 
 void corta_rgb(Img& imagem, Img& img_saida, int li, int ci, int lf, int cf) {
@@ -307,5 +343,80 @@ void borra_imagem(Img& img_in, Img& img_out) {
 }
 
 void equaliza_histograma(Img& img_in, Img& img_out) {
-  
+  img_out.qtd_linhas = img_in.qtd_linhas;
+  img_out.qtd_colunas = img_in.qtd_colunas;
+
+  int histograma_red[256] = {0};
+  int histograma_green[256] = {0};
+  int histograma_blue[256] = {0};
+
+  for (int i = 0; i < img_in.qtd_linhas; i++) {
+    for (int j = 0; j < img_in.qtd_colunas; j++) {
+      histograma_red[img_in.matriz[i][j].red] += 1;
+      histograma_green[img_in.matriz[i][j].green] += 1;
+      histograma_blue[img_in.matriz[i][j].blue] += 1;
+    }
+  }
+  int total_pixels = img_out.qtd_colunas * img_out.qtd_linhas;
+
+  for (int i = 0; i < img_out.qtd_linhas; i++) {
+    for (int j = 0; j < img_out.qtd_colunas; j++) {
+      int soma_histograma_red = 0;
+      int soma_histograma_blue = 0;
+      int soma_histograma_green = 0;
+
+      for (int k = 0; k <= img_in.matriz[i][j].red; k++) {
+        soma_histograma_red += histograma_red[k];
+      }
+
+      for (int k = 0; k <= img_in.matriz[i][j].green; k++) {
+        soma_histograma_green += histograma_green[k];
+      }
+
+      for (int k = 0; k <= img_in.matriz[i][j].blue; k++) {
+        soma_histograma_blue += histograma_blue[k];
+      }
+
+      Pixel pixel;
+      pixel.red = (soma_histograma_red * 255) / total_pixels;
+      pixel.green = (soma_histograma_green * 255) / total_pixels;
+      pixel.blue = (soma_histograma_blue * 255) / total_pixels;
+      img_out.matriz[i][j] = pixel;
+    }
+  }
+}
+
+void separa_canais(Img& img_in, Img& img_r, Img& img_g, Img& img_b) {
+  img_r.qtd_linhas = img_in.qtd_linhas;
+  img_r.qtd_colunas = img_in.qtd_colunas;
+
+  img_g.qtd_linhas = img_in.qtd_linhas;
+  img_g.qtd_colunas = img_in.qtd_colunas;
+
+  img_b.qtd_linhas = img_in.qtd_linhas;
+  img_b.qtd_colunas = img_in.qtd_colunas;
+
+  for (int i = 0; i < img_in.qtd_linhas; i++) {
+    for (int j = 0; j < img_in.qtd_linhas; j++) {
+      Pixel pixel_r;
+      Pixel pixel_g;
+      Pixel pixel_b;
+
+      pixel_r.red = img_in.matriz[i][j].red;
+      pixel_r.green = 0;
+      pixel_r.blue = 0;
+
+      pixel_g.red = 0;
+      pixel_g.green = img_in.matriz[i][j].green;
+      pixel_g.blue = 0;
+
+      pixel_b.red = 0;
+      pixel_b.green = 0;
+      pixel_b.blue = img_in.matriz[i][j].blue;
+
+      img_r.matriz[i][j] = pixel_r;
+      img_g.matriz[i][j] = pixel_g;
+      img_b.matriz[i][j] = pixel_b;
+    }
+  }
 }
